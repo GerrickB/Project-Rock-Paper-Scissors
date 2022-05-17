dcaad0b827815a2f9bc52595a195d7c1e99f529b
Gerrick B <lsgenether@gmail.com>
2022-05-16 18:00:45 -0700

    Add function to accept player and computer choice

--- javascript.js
+++ javascript.js
@@ -1,20 +1,17 @@
-// Returns either Rock, Paper or Scissors
+// Randomly returns either Rock, Paper or Scissors
 function computerPlay() {
-    const rock = 'Rock';
-    const paper = 'Paper';
-    const scissors = 'Scissors';
-
+    
     let choice = Math.floor(Math.random() * 3);
 
     switch (choice) {
         case 0:
-            console.log(rock);
+            console.log('ROCK');
             break;
         case 1:
-            console.log(paper);
+            console.log('PAPER');
             break;
         case 2:
-            console.log(scissors);
+            console.log('SCISSORS');
             break;
         default:
             console.log('ERROR');
@@ -23,4 +20,23 @@ function computerPlay() {
     console.log(choice);
 }
 
-computerPlay();
+// Accepts player and computer choice
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+
+    switch (true) {
+        case (player === 'ROCK') && (computer === 'PAPER'):
+            console.log('You Lose! Paper beats Rock');
+            break;
+        case (player === 'PAPER') && (computer === 'ROCK'):
+            console.log('You Won! Paper beats Rock');
+            break;
+        default:
+            console.log('ERROR');
+    }
+    
+}
+
+// computerPlay();
+playRound('Rock', 'Paper');
