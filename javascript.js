a2347b06c0d5b6d3f5eb168aafc26dc3f11e0459
Gerrick B <lsgenether@gmail.com>
2022-05-19 00:43:09 -0700

    Modify and clean game function

--- javascript.js
+++ javascript.js
@@ -51,8 +51,8 @@ function playRound(playerSelection, computerSelection) {
         case (player === 'SCISSORS') && (computer === 'ROCK'):
             return 'You Lose! Rock beats Scissors';
             break;
-        case (player === 'SCISSORS') && (computer === 'ROCK'):
-            return 'You Lose! Rock beats Scissors';
+        case (player === 'SCISSORS') && (computer === 'SCISSORS'):
+            return 'Tie';
             break;
         default:
             return 'ERROR';
@@ -62,18 +62,72 @@ function playRound(playerSelection, computerSelection) {
 
 // Determines who is the winner or loser
 function winCounter(playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+
+    switch (true) {
+        case (player === 'ROCK') && (computer === 'PAPER'):
+            return 'Lose';
+            break;
+        case (player === 'ROCK') && (computer === 'SCISSORS'):
+            return 'Win';
+            break;
+        case (player === 'ROCK') && (computer === 'ROCK'):
+            return 'Tie';
+            break;
+        
+        case (player === 'PAPER') && (computer === 'ROCK'):
+            return 'Win';
+            break;
+        case (player === 'PAPER') && (computer === 'SCISSORS'):
+            return 'Lose';
+            break;
+        case (player === 'PAPER') && (computer === 'PAPER'):
+            return 'Tie';
+            break;
+
+        case (player === 'SCISSORS') && (computer === 'PAPER'):
+            return 'Win';
+            break;
+        case (player === 'SCISSORS') && (computer === 'ROCK'):
+            return 'Lose';
+            break;
+        case (player === 'SCISSORS') && (computer === 'ROCK'):
+            return 'Tie';
+            break;
+        default:
+            return 'ERROR';
+    }
 
 }
 
 //
 function game() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0, wins = 0, loses = 0, ties = 0; i < 5; i++) {
         let playerSelection = prompt('Rock, Paper, or Scissors?')
         let computerSelection = computerPlay();
+        let result = winCounter(playerSelection, computerSelection);
 
         console.log((playRound(playerSelection, computerSelection)));
+
+        if (result === 'Win') {
+            wins++;
+        } else if (result === 'Lose') {
+            loses++;
+        } else {
+            ties++;
+        }
+
+        console.log('Wins: ' + wins + ' Loses: ' + loses + ' Tie: ' + ties)
     }
-}
+    // if (wins > loses) {
+    //     console.log('You won the game!');
+    // } else if (loses < wins) {
+    //     console.log('You lost the game');
+    // } else {
+    //     console.log('Tied')
+    // }
+    }
 
 game();
 
